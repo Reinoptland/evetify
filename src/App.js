6380976b30c16edf24568f78b904baabed09464e
Reinoptland <reinoptland@gmail.com>
2020-02-12 11:54:08 +0100

    Signup Route & Form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Signup from "./components/Signup";
 
 function Test() {
   return <h1>hello!</h1>;
@@ -12,7 +12,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Router>
+          <nav>
+            <Link to="/signup">
+              <li>Sign up!</li>
+            </Link>
+          </nav>
+
           <Route exact path="/" component={Test} />
+          <Route exact path="/signup" component={Signup} />
         </Router>
       </header>
     </div>
